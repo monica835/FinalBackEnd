9d16ba1be96822ba13bcaae1614f49f76a0b9919
xiekind <ziekind07@gmail.com>
2019-12-03 21:47:31 +0800

    fdsafdsafdsa

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path')
 //const Bcrypt = require("bcryptjs");
 const port = 5000
-// const multer =require("multer");
+const multer =require("multer");
 // const pusher = require('pusher');
+// const app = express();
+
+// const multer = require("multer");
+const cors = require("cors");
+
 const app = express();
+app.use(cors());
+
 
 
 // app.post('/multiupload',multiupload.single('file'),(req,res)=>{
@@ -31,12 +39,14 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // Parse requests of content-type - application/json
 app.use(bodyParser.json())
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-  next();
-});
+app.use('/static', express.static(path.join(__dirname, 'uploads')))
+
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+//   next();
+// });
 
 const dbConfig = 'mongodb://127.0.0.1:27017/abangDB';
 const db = mongoose.connection;
@@ -76,6 +86,39 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 // const Reservation = require('./models/model.reservation.js');
 // const Items = require('./models/model.item.js');
 //import controller
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = ["image/jpeg", "image/jpg", "image/png"];
+  if (!allowedTypes.includes(file.mimetype)) {
+    const error = new Error("Incorrect file");
+    error.code = "INCORRECT_FILETYPE";
+    return cb(error, false)
+  }
+  cb(null, true);
+}
+
+// const upload = multer({
+//   dest: './uploads',
+//   filename: function (req, file, cb) {
+//     //req.body is empty...
+//     //How could I get the new_file_name property sent from client here?
+//     cb(null, file.name+Date.now()+'.jpg');
+//   },
+//   fileFilter,
+//   limits: {
+//     fileSize: 5000000
+//   }
+// });
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads/')
+  },
+  filename: function (req, file, cb) {
+
+      cb(null,  file.fieldname+Date.now()+'.jpg' );
+
+  }
+});
+var upload = multer({ storage: storage });
 const createUser = require('./controller/accountsUsers.controller.js');
 //const test = require('./models/model.test.js');
 
@@ -113,144 +156,160 @@ app.post('/accountsUsers', function (req, res) {
 
 app.get("/login", createUser.AllUsers);
 
+app.post('/upload', upload.single('file'), (req, res) => {
+  res.json({ file: req.file });
+});
 
+
+
+app.use((err, req, res, next) => {
+  if (err.code === "INCORRECT_FILETYPE") {
+    res.status(422).json({ error: 'Only images are allowed' });
+    return;
+  }
+  if (err.code === "LIMIT_FILE_SIZE") {
+    res.status(422).json({ error: 'Allow file size is 500KB' });
+    return;
+  }
+});
 
 //GETTING USERS FROM DB
-app.get('/accountsUserList', (req, res) => {
-  AccountsUsers.find({}, (err, AccountsUsers) => {
-    if (err) {
-      res.send(err);
-    }
-    res.json({ AccountsUsers: AccountsUsers });
-  });
+// app.get('/accountsUserList', (req, res) => {
+//   AccountsUsers.find({}, (err, AccountsUsers) => {
+//     if (err) {
+//       res.send(err);
+//     }
+//     res.json({ AccountsUsers: AccountsUsers });
+//   });
 
-});
+// });
 
-//uploading files
+// //uploading files
 
-//ADDING PROVIDERS TO DB
-app.post('/accountsProvider', (req, res) => {
-  let accountsProviderToCreate = new AccountsProvider(
-    {
-      url: req.body.url,
-      companyName: req.body.companyName,
-      companyAddress: req.body.companyAddress,
-      email: req.body.email,
-      username: req.body.username,
-      password: req.body.password
-    }
-  );
-  accountsProviderToCreate.save((err, AccountsProvider) => {
-    if (err) {
-      res.send(err);
-    }
-    res.json(AccountsProvider);
-  });
+// //ADDING PROVIDERS TO DB
+// app.post('/accountsProvider', (req, res) => {
+//   let accountsProviderToCreate = new AccountsProvider(
+//     {
+//       url: req.body.url,
+//       companyName: req.body.companyName,
+//       companyAddress: req.body.companyAddress,
+//       email: req.body.email,
+//       username: req.body.username,
+//       password: req.body.password
+//     }
+//   );
+//   accountsProviderToCreate.save((err, AccountsProvider) => {
+//     if (err) {
+//       res.send(err);
+//     }
+//     res.json(AccountsProvider);
+//   });
 
-});
-
-
-//GETTIGN RPOVIDERS FRM DB
-app.get('/accountsProviderList', (req, res) => {
-  AccountsProvider.find({}, (err, accountsProvider) => {
-    if (err) {
-      res.send(err);
-    }
-    res.send({ accountsProvider: accountsProvider });
-  });
-});
-
-//ADDING RESERVATION TO DB
-app.post('/reservation', (req, res) => {
-  let reservationToCreate = new Reservation(
-    {
-      reservationID: req.body.reservationID,
-      accountID: req.body.accountID,
-      dateReserved: req.body.dateReserved,
-      dateReturned: req.body.dateReturned,
-      totalRate: req.body.totalRate,
-      status: req.body.status,
-    }
-  );
-  reservationToCreate.save((err, Reservation) => {
-    if (err) {
-      res.send(err);
-    }
-    res.json(Reservation);
-  });
-});
-
-//Getting reservation from DB
-app.get('/reservationList', (req, res) => {
-  Reservation.find({}, (err, reservation) => {
-    if (err) {
-      res.send(err);
-    }
-    res.send({ reservation: reservation });
-  });
-})
+// });
 
 
-//ADDDING ITEM TO DB
-app.post('/items', (req, res) => {
-  const ItemToCreate = new Items(
-    {
-      itemID: req.body.itemID,
-      itemName: req.body.itemName,
-      companyID: req.body.companyID,
-      category: req.body.category,
-      brand: req.body.brand,
-      model: req.body.model,
-      sitingcapacity: req.body.sitingcapacity,
-      color: req.body.color,
-      location: req.body.location,
-      rate: req.body.rate,
-      status: req.body.status,
+// //GETTIGN RPOVIDERS FRM DB
+// app.get('/accountsProviderList', (req, res) => {
+//   AccountsProvider.find({}, (err, accountsProvider) => {
+//     if (err) {
+//       res.send(err);
+//     }
+//     res.send({ accountsProvider: accountsProvider });
+//   });
+// });
 
-    }
-  );
-  ItemToCreate.save((err, items) => {
-    if (err) {
-      res.send(err);
-    }
-    res.json(items);
-  });
-});
+// //ADDING RESERVATION TO DB
+// app.post('/reservation', (req, res) => {
+//   let reservationToCreate = new Reservation(
+//     {
+//       reservationID: req.body.reservationID,
+//       accountID: req.body.accountID,
+//       dateReserved: req.body.dateReserved,
+//       dateReturned: req.body.dateReturned,
+//       totalRate: req.body.totalRate,
+//       status: req.body.status,
+//     }
+//   );
+//   reservationToCreate.save((err, Reservation) => {
+//     if (err) {
+//       res.send(err);
+//     }
+//     res.json(Reservation);
+//   });
+// });
 
-//Getting Items form DB
-app.get('/itemList', (req, res) => {
-  Items.find({}, (err, items) => {
-    if (err) {
-      res.send(err);
-    }
-    res.send({ items: items });
-  });
-});
+// //Getting reservation from DB
+// app.get('/reservationList', (req, res) => {
+//   Reservation.find({}, (err, reservation) => {
+//     if (err) {
+//       res.send(err);
+//     }
+//     res.send({ reservation: reservation });
+//   });
+// })
 
-app.post('/test', async (req, res) => {
-  const testCreate = new test(
-    {
-      password: req.body.password,
-      username: req.body.username
 
-    }
-  );
-  testCreate.save((err, test) => {
-    if (err) {
-      res.send(err);
-    }
-    res.json(test);
-  });
-});
+// //ADDDING ITEM TO DB
+// app.post('/items', upload.single("img"),(req, res) => {
+//   const ItemToCreate = new Items(
+//     {
+//       itemID: req.body.itemID,
+//       itemName: req.body.itemName,
+//       url:req.file.filename,
+//       companyID: req.body.companyID,
+//       category: req.body.category,
+//       brand: req.body.brand,
+//       model: req.body.model,
+//       sitingcapacity: req.body.sitingcapacity,
+//       color: req.body.color,
+//       location: req.body.location,
+//       rate: req.body.rate,
+//       status: req.body.status,
 
-app.get('/testGet', (req, res) => {
-  test.find({}, (err, test) => {
-    if (err) {
-      res.send(err);
-    }
-    res.send({ test: test });
-  });
-});
+//     }
+//   );
+//   ItemToCreate.save((err, items) => {
+//     if (err) {
+//       res.send(err);
+//     }
+//     res.json(items);
+//   });
+// });
+
+// //Getting Items form DB
+// app.get('/itemList', (req, res) => {
+//   Items.find({}, (err, items) => {
+//     if (err) {
+//       res.send(err);
+//     }
+//     res.send({ items: items });
+//   });
+// });
+
+// app.post('/test', async (req, res) => {
+//   const testCreate = new test(
+//     {
+//       password: req.body.password,
+//       username: req.body.username
+
+//     }
+//   );
+//   testCreate.save((err, test) => {
+//     if (err) {
+//       res.send(err);
+//     }
+//     res.json(test);
+//   });
+// });
+
+// app.get('/testGet', (req, res) => {
+//   test.find({}, (err, test) => {
+//     if (err) {
+//       res.send(err);
+//     }
+//     res.send({ test: test });
+//   });
+// });
 
 
 // ------------------------------------------------------------
--- models/model.item.js
+++ models/model.item.js
@@ -3,17 +3,18 @@ const Schema = mongoose.Schema;
 
 const ItemSchema = new Schema(
     {
-        itemID: {type: String, require: true},
-        itemName:{type: String, require: true},
-        companyID: {type: String, require:true},
-        category:{type:String, require: true},
-        brand:{type:String, require: true},
-        model:{type:String, require: true},
-        sitingcapacity:{type:Number, require: false},
-        color:{type:String, require: false},
-        location:{type:String, require: true},
-        rate:{type:Number, require: true},
-        status:{type:Boolean, require: true},
+        itemID: {type: String, required: true},
+        itemName:{type: String, required: true},
+        url:{type:String,required:true},
+        companyID: {type: String, required:true},
+        category:{type:String, required: true},
+        brand:{type:String, required: true},
+        model:{type:String, required: true},
+        sitingcapacity:{type:Number, required: false},
+        color:{type:String, required: false},
+        location:{type:String, required: true},
+        rate:{type:Number, required: true},
+        status:{type:Boolean, required: true},
 
     }
 
